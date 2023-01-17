fbc04ded044a44f81624b27d4728ee3c4a30733e
AshkanArabim <ashkan.arabim@gmail.com>
2023-01-16 17:08:10 -0700

    add table header

--- static/script.js
+++ static/script.js
@@ -1,122 +1,126 @@
-const getBtn = document.querySelector("#gettweets");
-const tweetholder = document.querySelector("#tweets");
-const fetchurl = "http://localhost:3000";
+(() => {
+  const tweetholder = document.querySelector("#tweets");
+  const updateBtn = document.querySelector("#gettweets");
+  const port = 3000;
+  const fetchurl = `http://localhost:${port}`;
 
-class utils {
-  static cr(element) {
-    return document.createElement(element);
-  }
-  static qs(query) {
-    return document.querySelector(query);
-  }
-  static appChildren(parent) {
-    for (let i = 1; i <= arguments.length - 1; i++) {
-      parent.appendChild(arguments[i]);
+  updateBtn.addEventListener("click", update);
+
+  class utils {
+    static cr(element) {
+      return document.createElement(element);
     }
-  }
-  static etc(element, text) {
-    const product = this.cr(element);
-    product.textContent = text;
-    if (arguments[2]) {
-      for (let i = 2; i < arguments.length; i++) {
-        product.classList.add(arguments[i]);
+    static qs(query) {
+      return document.querySelector(query);
+    }
+    static appChildren(parent) {
+      for (let i = 1; i <= arguments.length - 1; i++) {
+        parent.appendChild(arguments[i]);
       }
     }
-    return product;
-  }
-}
-
-function resetTable() {
-  tweetholder.textContent = "";
-}
-
-function renderTable(data) {
-  // tweet satus modifier functions
-  async function modStatus(updatedStatus) {} // TODO
-
-  // reset table
-  resetTable();
-
-  //populate first row
-  const indexRow = document.createElement("tr");
-  tweetholder.appendChild(indexRow);
-
-  for (key of Object.keys(data[0])) {
-    const indexCell = document.createElement("td");
-    indexCell.textContent = key;
-  }
-
-  //render data for each row
-  for (row of data) {
-    const tr = document.createElement("tr");
-
-    let firstItr = true;
-    let id;
-
-    // loop for data fields
-    // iterates over columns
-    for (cellData of Object.values(row)) {
-      if (firstItr) {
-        id = cellData;
-        firstItr = false;
+    static etc(element, text) {
+      const product = this.cr(element);
+      product.textContent = text;
+      if (arguments[2]) {
+        for (let i = 2; i < arguments.length; i++) {
+          product.classList.add(arguments[i]);
+        }
       }
+      return product;
+    }
+  }
 
-      const td = document.createElement("td");
-      td.textContent = cellData;
-      tr.appendChild(td);
+  function resetTable() {
+    tweetholder.textContent = "";
+  }
+
+  function renderTable(data) {
+    // TODO: render table header
+    resetTable();
+
+    //populate first row
+    const indexRow = document.createElement("tr");
+    tweetholder.appendChild(indexRow);
+
+    for (key of Object.keys(data[0])) {
+      const indexCell = document.createElement("td");
+      console.log(indexCell);
+      indexCell.textContent = key;
+      indexRow.appendChild(indexCell);
     }
 
-    // add buttons for scam classification
-    const actions = utils.etc("td", "please choose your opinion:");
-    const scamBtn = utils.etc("button", "This is a scam", "scamBtn");
-    const notScamBtn = utils.etc("button", "NOT a scam", "notScamBtn");
-    utils.appChildren(actions, scamBtn, notScamBtn);
+    //render data for each row
+    for (row of data) {
+      const tr = document.createElement("tr");
 
-    // assign functions to response buttons
-    scamBtn.addEventListener("click", () => {
-      setTweetStatus("yes", id);
-    });
-    notScamBtn.addEventListener("click", () => {
-      setTweetStatus("no", id);
-    });
+      let firstItr = true;
+      let id;
 
-    tr.appendChild(actions);
+      // iterates over columns
+      for (cellData of Object.values(row)) {
+        if (firstItr) {
+          id = cellData;
+          firstItr = false;
+        }
 
-    tweetholder.appendChild(tr);
+        const td = document.createElement("td");
+        td.textContent = cellData;
+        tr.appendChild(td);
+      }
+
+      const actions = utils.etc("td", "please choose your opinion:");
+      const scamBtn = utils.etc("button", "This is a scam", "scamBtn");
+      const notScamBtn = utils.etc("button", "NOT a scam", "notScamBtn");
+
+      scamBtn.addEventListener("click", () => {
+        sendFeedback("yes", id);
+        renderTable(data);
+        update();
+      });
+      notScamBtn.addEventListener("click", () => {
+        sendFeedback("no", id);
+        renderTable(data);
+        update();
+      });
+
+      utils.appChildren(actions, scamBtn, notScamBtn);
+      tr.appendChild(actions);
+      tweetholder.appendChild(tr);
+    }
   }
-}
 
-async function getTweets() {
-  const response = await fetch(fetchurl + "/tweets", {
-    method: "GET",
-  });
-  return response.json();
-}
+  async function getTweets() {
+    const response = await fetch(fetchurl + "/tweets", {
+      method: "GET",
+    });
+    return response.json();
+  }
 
-async function setTweetStatus(word, id) {
-  const data = {
-    word: word,
-    id: id,
-  };
+  async function sendFeedback(word, id) {
+    const data = {
+      word: word,
+      id: id,
+    };
 
-  // DEBUG
-  console.log(data);
-  console.log(JSON.stringify(data));
+    // DEBUG
+    console.log(data);
+    console.log(JSON.stringify(data));
 
-  // send string to the backend.
-  const response = await fetch(`${fetchurl}/response`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+    // send string to the backend.
+    const response = await fetch(`${fetchurl}/response`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-  return response;
-}
+    return response;
+  }
 
-getBtn.addEventListener("click", async () => {
-  tweets = await getTweets();
-  console.log(tweets);  
-  renderTable(tweets);
-});
+  async function update() {
+    tweets = await getTweets();
+    console.log(tweets);
+    renderTable(tweets);
+  }
+})();
